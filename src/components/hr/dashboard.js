7a6f7828be9715b9f1f6e4550b3fe48fe4ed8b0b
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-03-13 23:10:03 +0600

    temperature conver and simple form control

--- src/components/hr/dashboard.js
+++ src/components/hr/dashboard.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Calculator from '../lifting-state/calculator';
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>Dashboard</div>
+      <div>
+        <Calculator />
+      </div>
     )
   }
 }
--- src/components/hr/employees.js
+++ src/components/hr/employees.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Test from '../test';
 
 export default class Employees extends Component {
   render() {
     return (
-      <div>Employees</div>
+      <div>Employees
+          <Test />
+      </div>
     )
   }
 }
